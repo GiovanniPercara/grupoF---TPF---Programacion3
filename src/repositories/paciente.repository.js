d131d7ca957a77a847f77d5884c2fa25d10675d3
Estrella Cardozo <estrellacardozoit@gmail.com>
2026-05-08 04:00:02 -0300

    Modificacion paciente repository primera entrega

--- src/repositories/paciente.repository.js
+++ src/repositories/paciente.repository.js
@@ -7,7 +7,52 @@ const findByUsuarioId = async (id_usuario) => {
   );
   return rows[0] || null;
 };
+//APLICANDO BREAD PACIENTES - PRIMERA ENTREGA 
+// Se enlista todos los pacientes
+const findAll = async () => {
+  const [rows] = await pool.query('SELECT * FROM v_pacientes');
+  return rows;
+};
 
+//  Obtiene un solo paciente.
+const findById = async (id) => {
+  const [rows] = await pool.query('SELECT * FROM v_pacientes WHERE id_paciente = ?', [id]);
+  return rows || null;
+};
+
+// Crear nuevo paciente
+const save = async (datos) => {
+  const { id_usuario, id_obra_social } = datos;
+  const [result] = await pool.query(
+    'INSERT INTO pacientes (id_usuario, id_obra_social) VALUES (?, ?)',
+    [id_usuario, id_obra_social]
+  );
+  return result.insertId;
+};
+
+// Modifica obra social del paciente
+const update = async (id, datos) => {
+  const { id_obra_social } = datos;
+  const [result] = await pool.query(
+    'UPDATE pacientes SET id_obra_social = ? WHERE id_paciente = ?',
+    [id_obra_social, id]
+  );
+  return result.affectedRows > 0;
+};
+
+// Borrado lógico (Soft Delete)
+const softDelete = async (id) => {
+  const sql = `UPDATE usuarios u 
+               JOIN pacientes p ON u.id_usuario = p.id_usuario 
+               SET u.activo = 0 WHERE p.id_paciente = ?`;
+  const [result] = await pool.query(sql, [id]);
+  return result.affectedRows > 0;
+};
 export {
-  findByUsuarioId
+  findByUsuarioId,
+  findAll,
+  findById,
+  save,
+  update,
+  softDelete
 };
