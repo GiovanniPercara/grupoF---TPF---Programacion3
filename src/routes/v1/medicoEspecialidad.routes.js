4019b0a12afcd0663c2918299d8e5d14acc34965
Lucía <lucyallassia12@gmail.com>
2026-06-09 11:22:06 -0300

    corrección medicoEspecialidad

--- src/routes/v1/medicoEspecialidad.routes.js
+++ src/routes/v1/medicoEspecialidad.routes.js
@@ -3,7 +3,7 @@ import express from 'express';
 import { asociarMedicoEspecialidadController } from '../../controllers/medicoEspecialidad.controller.js';
 import { asociarMedicoEspecialidadValidator } from '../../middlewares/medicoEspecialidad.validator.js';
 import { verificarToken } from '../../middlewares/auth.middleware.js';
-import { authorize } from '../../middlewares/role.middleware.js';
+import { soloAdmin } from '../../middlewares/admin.middleware.js';
 import validate from '../../middlewares/validate.js';
 
 const router = express.Router();
@@ -12,7 +12,7 @@ const router = express.Router();
 router.put(
   '/',
   verificarToken,
-  authorize(3),
+  soloAdmin,
   asociarMedicoEspecialidadValidator,
   validate,
   asociarMedicoEspecialidadController
