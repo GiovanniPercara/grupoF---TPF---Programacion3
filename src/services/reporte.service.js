1d638d65cfbf4ca964da410a08ea466c0081e67f
Estrella Cardozo <estrellacardozoit@gmail.com>
2026-06-27 01:15:05 -0300

    Se corrige reporte service y repository para correcta comunicación con la base de datos. Se modifica README.

--- src/services/reporte.service.js
+++ src/services/reporte.service.js
@@ -1,47 +1,8 @@
 import PDFDocument from "pdfkit";
-import pool from "../config/db.js";
+import { obtenerTurnosPaciente } from "../repositories/reporte.repository.js";
 
 const generarReportePaciente = async (idPaciente, res) => {
-  const [turnos] = await pool.query(
-    `
-    SELECT
-      tr.id_turno_reserva,
-      tr.fecha_hora,
-      tr.valor_total,
-      tr.atentido AS atendido,
-
-      up.nombres AS paciente_nombre,
-      up.apellido AS paciente_apellido,
-      up.documento,
-
-      um.nombres AS medico_nombre,
-      um.apellido AS medico_apellido,
-
-      e.nombre AS especialidad
-
-    FROM turnos_reservas tr
-
-    INNER JOIN pacientes p
-      ON tr.id_paciente = p.id_paciente
-
-    INNER JOIN usuarios up
-      ON p.id_usuario = up.id_usuario
-
-    INNER JOIN medicos m
-      ON tr.id_medico = m.id_medico
-
-    INNER JOIN usuarios um
-      ON m.id_usuario = um.id_usuario
-
-    INNER JOIN especialidades e
-      ON m.id_especialidad = e.id_especialidad
-
-    WHERE tr.id_paciente = ?
-
-    ORDER BY tr.fecha_hora
-    `,
-    [idPaciente],
-  );
+  const turnos = await obtenerTurnosPaciente(idPaciente);
 
   if (turnos.length === 0) {
     throw new Error("El paciente no posee turnos registrados");
